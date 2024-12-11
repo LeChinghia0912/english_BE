098248a9ebb79151d7da500e4a31a643cc9aeb9a
Lê Chí Nghĩa <nghia520414@gmail.com>
2024-12-12 00:38:42 +0700

    dev

--- configs/db.js
+++ configs/db.js
@@ -3,16 +3,17 @@ const mongoose = require("mongoose");
 
 async function connect() {
   try {
-    // await mongoose.connect("mongodb://localhost/english", {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    //   serverSelectionTimeoutMS: 5000,
-    // });
-    await mongoose.connect("mongodb://localhost:27017/english", {});
+    // Thay đổi URL để kết nối với MongoDB Atlas
+    const dbUri = "mongodb+srv://nghia3009:nghia3009@cluster0.1cshm.mongodb.net/";
+    await mongoose.connect(dbUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Thời gian chờ tối đa khi kết nối
+    });
 
-    console.log("Kêt nối DB thành công");
+    console.log("Kết nối DB thành công");
   } catch (error) {
-    console.log("🚀 ~ connect ~ error:", error)
+    console.log("🚀 ~ connect ~ error:", error);
     console.log("Kết nối thất bại");
     process.exit(1);
   }
